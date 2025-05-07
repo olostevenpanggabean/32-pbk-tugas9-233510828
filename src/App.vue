3171567c5cf68e4ca7be72095fb771a9a0c6eee7
olo steven <olostevenpanggabean@student.uir.ac.id>
2025-05-08 01:21:11 +0700

    Props

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Parent Component</h2>
+    <Child :message="parentMessage" />
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+import Child from './components/Child.vue'
+
+const parentMessage = ref('Halo dari Parent!')
+
+onMounted(() => {
+  console.log('[Parent][Props] mengirim prop `message`:', parentMessage.value)
+})
+</script>
---
+++ src/components/Child.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>Child Component</h3>
+    <p>Pesan dari Parent: {{ message }}</p>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+
+const props = defineProps({
+  message: String
+})
+
+onMounted(() => {
+  console.log('[Child][Props] menerima prop `message`:', props.message)
+})
+</script>
